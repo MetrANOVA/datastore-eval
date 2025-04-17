1584274526f6915200b5b37d7f52c449ee9edbce
James Kafader <jkafader@es.net>
2025-04-17 18:24:17 +0000

    add timings and final report for imports

--- timescaledb/insert.py
+++ timescaledb/insert.py
@@ -43,15 +43,44 @@ managers = {
 }
 
 timing_buckets = {
-    "values": 0.0,
-    "metadata": 0.0,
+    "values_insert": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    },
+    "metadata_insert": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    },
+    "values_assembly": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    },
+    "metadata_assembly": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    }
 }
 
-def timed_copy(mgr, batch, timing_bucket="values"):
+def timed_copy(mgr, batch, timing_bucket="values_insert"):
     before = time.perf_counter()
     mgr.copy(batch)
+    # timing details
     after = time.perf_counter()
-    timing_buckets[timing_bucket] += after - before
+    execution_time = after - before
+    timing_buckets[timing_bucket]["total"] += execution_time
+    if timing_buckets[timing_bucket]["min"] is None or execution_time < timing_buckets[timing_bucket]["min"]:
+        timing_buckets[timing_bucket]["min"] = execution_time
+    if timing_buckets[timing_bucket]["max"] is None or execution_time > timing_buckets[timing_bucket]["max"]:
+        timing_buckets[timing_bucket]["max"] = execution_time
+    timing_buckets[timing_bucket]["count"] += 1
     
 def insert_batch(batch, strategy="hashed-metadata"):
     if strategy == "hashed-metadata":
@@ -62,42 +91,109 @@ def insert_batch(batch, strategy="hashed-metadata"):
             cur.execute("CREATE TEMP TABLE tmp_table (data jsonb) ON COMMIT DROP")
             # do COPY
             mgr = CopyManager(conn, 'tmp_table', ("data",))
-            timed_copy(mgr, metadata_batch, timing_bucket="metadata")
+            timed_copy(mgr, metadata_batch, timing_bucket="metadata_insert")
             # insert from temp table into the metadata table
             cur.execute("INSERT INTO %s SELECT * FROM tmp_table ON CONFLICT DO NOTHING" % args.metadata_table)
         # end transaction
         logging.info('copied %s metadata rows (postgres insert time)' % len(batch))
         conn.commit()
         logging.info('committed %s metadata rows (postgres overhead)' % len(batch))
-        timed_copy(managers['values'], batch, timing_bucket="values")
+        timed_copy(managers['values'], batch, timing_bucket="values_insert")
         logging.info('copied %s values rows (postgres insert time)' % len(batch))
     if strategy == "inline-metadata":
-        timed_copy(managers['values'], batch, timing_bucket="values")
+        timed_copy(managers['values'], batch, timing_bucket="values_insert")
         logging.info('copied %s values rows (postgres insert time)' % len(batch))
+
+def timed_assembly(infile, header, batch_size=1, timing_bucket="values_assembly"):
+    batch = []
+    before = time.perf_counter()
+    for line in infile:
+        row = line.rstrip("\n").split("\t")
+
+        batch.append(assemble(row, header, fmt=WIDE_FORMAT if args.wide else NARROW_FORMAT, original_line=line))
+        if len(batch) == args.batch_size:
+            logging.info('assembled %s values rows (python assembly overhead)' % args.batch_size)
+            
+            # timing details
+            after = time.perf_counter()
+            execution_time = after - before
+            timing_buckets[timing_bucket]["total"] += execution_time
+            if timing_buckets[timing_bucket]["min"] is None or execution_time < timing_buckets[timing_bucket]["min"]:
+                timing_buckets[timing_bucket]["min"] = execution_time
+            if timing_buckets[timing_bucket]["max"] is None or execution_time > timing_buckets[timing_bucket]["max"]:
+                timing_buckets[timing_bucket]["max"] = execution_time
+            timing_buckets[timing_bucket]["count"] += 1
+            yield batch
+            batch = []
+            before = time.perf_counter()
+                
+    # yield the last incomplete batch, don't both with min/max statistics
+    after = time.perf_counter()
+    timing_buckets[timing_bucket]["total"] += execution_time
+    timing_buckets[timing_bucket]["count"] += 1
+    yield batch
+
+def final_report():
+    batch_stats_template = """
+    Batch Statistics (%s):
+        Total batches: %s
+        Rows per batch: %s
+        Average batch conversion/assembly overhead: %.2fs
+        Average batch insertion time: %.2fs
+        Min batch insertion time: %.2fs
+        Max batch insertion time: %.2fs
+    """
+    total_times_template = """
+    Total insertion time (%s): %.2fs
+    """
+
+    reports = ["values"]
+    if args.strategy == "":
+        reports.append("metadata")
+
+    batch_stats = ""
+    total_times = ""
+    for report in reports:
+        batch_stats += batch_stats_template % (
+            report,
+            timing_buckets["%s_assembly" % report]["count"],
+            args.batch_size,
+            timing_buckets["%s_assembly" % report]["total"] / timing_buckets["%s_assembly" % report]["count"],
+            timing_buckets["%s_insert" % report]["total"] / timing_buckets["%s_insert" % report]["count"],
+            timing_buckets["%s_insert" % report]["min"],
+            timing_buckets["%s_insert" % report]["max"],
+        )
+        total_times += total_times_template % (
+            report,
+            timing_buckets["%s_insert" % report]["total"]
+        )
+
+    avg_insertion_rate = total_inserts / timing_buckets["values_insert"]["total"]
     
+    print("""
+
+    --- Insertion Summary ---
+    Total rows processed: %s
+    %s
+    Average insertion rate: %.2fs rows/sec
+    %s
+    """ % (total_inserts, total_times, avg_insertion_rate, batch_stats))
+
+        
 header_line = args.infile.readline()
 header = header_line.strip().split("\t")
 total_inserts = 0
-batch = []
-for line in args.infile:
-    row = line.rstrip("\n").split("\t")
-    batch.append(assemble(row, header, fmt=WIDE_FORMAT if args.wide else NARROW_FORMAT, original_line=line))
-    if len(batch) == args.batch_size:
-        logging.info('assembled %s values rows (python assembly overhead)' % args.batch_size)
-        insert_batch(batch, strategy=args.strategy)
-        total_inserts += len(batch)
-        if total_inserts >= args.limit:
-            conn.commit()
-            logging.info('committed %s values rows (postgres overhead)' % args.limit)
-            conn.close()
-            sys.exit()
-        batch = []
-# finish last batch
-total_inserts += len(batch)
-insert_batch(batch, strategy=args.strategy)
 
-# 
+for batch in timed_assembly(infile=args.infile, header=header, batch_size=args.batch_size, timing_bucket="values_assembly"):
+    insert_batch(batch, strategy=args.strategy)
+    total_inserts += len(batch)
+    if total_inserts >= args.limit:
+        conn.commit()
+        logging.info('committed %s values rows (postgres overhead)' % args.limit)
+        break
+
 # don't forget to commit!
 conn.commit()
 logging.info('committed %s values rows (postgres overhead)' % total_inserts)
 conn.close()
+final_report()
