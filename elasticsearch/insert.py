54e75b443b14ac97de699414e5db3dded6cada80
James Kafader <jkafader@es.net>
2025-07-11 11:33:05 -0700

    initial version of elasticsearch bulk inserter

---
+++ elasticsearch/insert.py
@@ -0,0 +1,197 @@
+import argparse
+import csv
+import json
+import datetime
+from mappings import NARROW_FORMAT, WIDE_FORMAT
+from datetime import datetime
+from assemble import assemble
+import logging
+import tempfile
+import os
+import random
+import string
+import hashlib
+import pickle
+
+parser = argparse.ArgumentParser(description='Inserts ESnet Stardust Data into timescaledb, producing a timing summary report.')
+
+# connection
+parser.add_argument('--user', help='Elasticsearch username', default='timescale')
+parser.add_argument('--password', help='Elasticsearch User Password', default='timescale')
+parser.add_argument('--host', help="remote elastic host")
+parser.add_argument('--port', help="remote elastic port")
+
+# indexes to use
+parser.add_argument('--values-index', help="Table to insert values into. Receives both values and metadata (in 'metadata' column) when strategy is 'inline-metadata'", default='values')
+parser.add_argument('--scoreboard-index', help="Table to insert metrics/scoreboard info.", default='scoreboard')
+
+# partitioning and insert options
+parser.add_argument('--total-partitions', help="use consistent hash partitioning to partition binary output results", type=int, default=0)
+parser.add_argument('--partition', help="the binary output partition to prepare", type=int)
+parser.add_argument('--skip', help="Only insert every Nth row", type=int, default=1)
+parser.add_argument('--offset', help="offset to begin inserts from from input file", type=int, default=0)
+parser.add_argument('--limit', help="total insertion limit", type=int, default=20000)
+parser.add_argument('--batch-size', help="Batch size to do inserts, in rows.", type=int, default=5000)
+
+# input file and format
+parser.add_argument('--infile', help="Read rows from infile. Default: sys.stdin", default=sys.stdin, type=argparse.FileType('r'))
+parser.add_argument('--wide', help="Use stardust 'wide' format, including all columns.", action='store_true')
+
+# intermediate transform output
+parser.add_argument('--transform-output-dir', help="directory name to output binary COPY statements to", default="/tmp/%s" % ''.join(random.choices(string.ascii_letters + string.digits, k=8)))
+parser.add_argument('--transform-output-intermediate', help="save binary intermediate output. See also: --binary-output-dir. default: False", action='store_true')
+parser.add_argument('--transform-input-dir', help="read COPY batches fron binary intermediate input. See also: --binary-output-intermediate.")
+
+args = parser.parse_args()
+
+es_client = Elasticsearch(host=args.host, port=args.port, basic_auth=('user', 'pass'))
+
+col_source = NARROW_FORMAT
+if args.wide:
+    col_source = WIDE_FORMAT
+
+timing_buckets = {
+    "insert": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    },
+    "write_transformed": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    },
+    "assembly": {
+        "total": 0.0,
+        "count": 0,
+        "min": None,
+        "max": None,
+    },
+}
+
+# Assemble document rows
+
+def timed_assembly(infile, header, batch_size=1, timing_bucket="assembly", offset=0):
+    batch = []
+    before = time.perf_counter()
+    curr_line = 0
+    for line in infile:
+        if curr_line < offset or ((curr_line - offset) % args.skip) != 0:
+            if curr_line % 1000 == 0:
+                logging.info("seeking to offset... %s", curr_line)
+            elif curr_line < 1000 and ((curr_line - offset) % args.skip) != 0:
+                logging.info("skipping line %s. Offset: %s Skip: %s Curr_line - offset: %s Curr_line - offset mod skip: %s" % (curr_line, offset, args.skip, (curr_line - offset), ((curr_line - offset) % args.skip) ))
+            curr_line += 1
+            continue
+        curr_line += 1
+        row = line.rstrip("\n").split("\t")
+        if args.total_partitions:
+            hash_output = hash_row(row)
+            hash_bucket = int(hash_output, 16) % args.total_partitions
+            if hash_bucket != args.partition:
+                continue
+            if curr_line % 1000 == 0:
+                logging.info("row %s: partition %s" % (curr_line, hash_bucket))
+
+        batch.append(assemble(row, header, fmt=WIDE_FORMAT if args.wide else NARROW_FORMAT, original_line=line))
+        if len(batch) == args.batch_size:
+            logging.info('assembled %s values rows (python assembly overhead)' % args.batch_size)
+            
+            # timing details
+            after = time.perf_counter()
+            execution_time = after - before
+            timing_buckets[timing_bucket]["total"] += execution_time
+            if timing_buckets[timing_bucket]["min"] is None or execution_time < timing_buckets[timing_bucket]["min"]:
+                timing_buckets[timing_bucket]["min"] = execution_time
+            if timing_buckets[timing_bucket]["max"] is None or execution_time > timing_buckets[timing_bucket]["max"]:
+                timing_buckets[timing_bucket]["max"] = execution_time
+            timing_buckets[timing_bucket]["count"] += 1
+            yield batch
+            batch = []
+            before = time.perf_counter()
+                
+    # yield the last incomplete batch, don't both with min/max statistics
+    after = time.perf_counter()
+    timing_buckets[timing_bucket]["total"] += execution_time
+    timing_buckets[timing_bucket]["count"] += 1
+    yield batch
+
+# Deal with details of transformed output
+
+def get_file():
+    fname = "%s.transformed.%s" % (str(get_file.calls).zfill(8), get_file.suffix)
+    get_file.calls += 1
+    return open(os.path.join(get_file.dirname, fname), "wb+")
+
+def tmpfile_factory(preserve_copy_files, suffix="json"):
+    tmpfile_factory = lambda: tempfile.NamedTemporaryFile(suffix=suffix, delete=False)
+    def tmpdir(dirname, suffix):
+        if not hasattr(get_file, 'calls'):
+            get_file.calls = 0
+            get_file.dirname = dirname
+        get_file.suffix = suffix
+        return get_file
+    if preserve_copy_files:
+        tmpfile_factory = tmpdir(args.binary_output_dir, suffix)
+    return tmpfile_factory
+
+def timed_write_transformed(mgr, batch, timing_bucket="write_transformed", factory=tmpfile_factory(False)):
+    before = time.perf_counter()
+    tmpfile = factory()
+    pickle.dump(batch, tmpfile, protocol=pickle.HIGHEST_PROTOCOL)
+    # timing details
+    after = time.perf_counter()
+    execution_time = after - before
+    timing_buckets[timing_bucket]["total"] += execution_time
+    if timing_buckets[timing_bucket]["min"] is None or execution_time < timing_buckets[timing_bucket]["min"]:
+        timing_buckets[timing_bucket]["min"] = execution_time
+    if timing_buckets[timing_bucket]["max"] is None or execution_time > timing_buckets[timing_bucket]["max"]:
+        timing_buckets[timing_bucket]["max"] = execution_time
+    timing_buckets[timing_bucket]["count"] += 1
+    tmpfile = open(tmpfile.name, 'rb')
+    return tmpfile
+
+def timed_bulk_insert(f, timing_bucket="insert"):
+    batch = pickle.load(f)
+    before = time.perf_counter()
+    before_timestamp = datetime.now()
+    es_client.bulk(batch, index=args.values_index)
+    logging.info('.bulk() %s rows (elasticsearch insert time)' % args.batch_size)
+    after = time.perf_counter()
+    after_timestamp = datetime.now()
+    execution_time = after - before
+    timing_buckets[timing_bucket]["total"] += execution_time
+    if timing_buckets[timing_bucket]["min"] is None or execution_time < timing_buckets[timing_bucket]["min"]:
+        timing_buckets[timing_bucket]["min"] = execution_time
+    if timing_buckets[timing_bucket]["max"] is None or execution_time > timing_buckets[timing_bucket]["max"]:
+        timing_buckets[timing_bucket]["max"] = execution_time
+    timing_buckets[timing_bucket]["count"] += 1
+    es_client.index(index=args.scoreboard_index, {
+        "index_name": args.values_index, 
+        "batch_size": args.batch_size,
+        "start_time": before_timestamp.isoformat(),
+        "end_time": after_timestamp.isoformat()
+    })
+
+def hash_row(row):
+    ROUTER_IDX = 19
+    PORT_IDX = 34
+    if args.wide:
+        ROUTER_IDX = 605
+        PORT_IDX = 610
+    port_string = "%s::%s" % (row[ROUTER_IDX], row[PORT_IDX])
+    return hashlib.md5(port_string.encode('UTF-8')).hexdigest()
+
+total_inserts = 0
+
+if args.transform_input_dir:
+    for filename in sorted(os.listdir(args.transform_input_dir)):
+        with open(os.path.join(args.transform_input_dir, filename), 'rb') as f:
+            timed_bulk_insert(f)
+            total_inserts += args.batch_size
+else:
+    header_line = args.infile.readline()
+    header = header_line.strip().split("\t")
+    in timed_assembly(infile=args.infile, header=header, batch_size=args.batch_size, timing_bucket="values_assembly", offset=args.offset)
